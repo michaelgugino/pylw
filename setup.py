7a9bd3f0e5660e2de3842a19d4588baf99f47fe4
Michael <michael_gugino@cable.comcast.com>
2015-12-07 17:50:20 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pylw',
+      version='0.1',
+      description='The Python Lightweight Web Framework',
+      url='http://github.com/michaelgugino/pylw',
+      author='Michael Gugino',
+      author_email='mike@funwithlinux.net',
+      license='GPLv3',
+      packages=['pylw'],
+      zip_safe=False)

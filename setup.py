60ebe6ceda97ef9e3d6c1604930a6ad31d9b02d6
Michael <michael_gugino@cable.comcast.com>
2015-12-09 18:10:39 -0500

    added pytest to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,18 @@
 from setuptools import setup
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+import sys
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errcode = pytest.main(self.test_args)
+        sys.exit(errcode)
 
 setup(name='pylw',
       version='0.1',
@@ -6,6 +20,8 @@ setup(name='pylw',
       url='http://github.com/michaelgugino/pylw',
       author='Michael Gugino',
       author_email='mike@funwithlinux.net',
+      tests_require=['pytest'],
+      cmdclass={'test': PyTest},
       license='GPLv3',
       packages=['pylw'],
       zip_safe=False)

d82942a1c5e971aad3879278661664c9c248c4cc
Michael <michael_gugino@cable.comcast.com>
2015-12-11 20:56:55 -0500

    Add cython support in setup.py and tox

--- README.rst
+++ README.rst
@@ -1,7 +1,20 @@
+pylw
+======
+Python Lightweight Webframework.
+
+|Build Status|
+
 This is a somewhat useful web framework.
 
 It's meant to be fast, and to do very little.
 
 Unicode support...none.
+~~~~~~~~~~~~
+We also don't support python3 at this time.
 
 Complexity...zero.
+~~~~~~~~~~~~
+
+
+.. |Build Status| image:: https://travis-ci.org/michaelgugino/pylw.svg?branch=master
+   :target: https://travis-ci.org/michaelgugino/pylw
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@
 from setuptools import setup
 from setuptools import setup, find_packages, Extension
 from setuptools.command.test import test as TestCommand
-import sys, io
+import sys, io, os, glob
 
 module1 = Extension('pylw.qs_parse', sources = ['pylw/qs_parse.c'])
 module2 = Extension('pylw.route_find', sources = ['pylw/route_find.c'])
@@ -31,10 +31,42 @@ class PyTest(TestCommand):
         errcode = pytest.main(self.test_args)
         sys.exit(errcode)
 
+#Cython Support
+try:
+    from Cython.Distutils import build_ext
+    CYTHON = True
+except ImportError:
+    CYTHON = False
+
+if CYTHON:
+    def list_modules(dirname):
+        filenames = glob.glob(os.path.join(dirname, '*.py'))
+
+        module_names = []
+        for name in filenames:
+            module, ext = os.path.splitext(os.path.basename(name))
+            if module != '__init__':
+                module_names.append(module)
+
+        return module_names
+
+    ext_modules = [
+        Extension('pylw.' + ext, [os.path.join('pylw', ext + '.py')])
+        for ext in list_modules(os.path.join('.', 'pylw'))]
+
+    ext_modules.append(module1)
+    ext_modules.append(module2)
+
+    cmdclass = {'build_ext': build_ext, 'test': PyTest}
+
+else:
+    cmdclass = {'test': PyTest}
+    ext_modules = [module1, module2]
+
 setup(name='pylw',
       version='0.1',
       description='The Python Lightweight Web Framework',
-      #long_description=io.open('README.rst', 'r', encoding='utf-8').read(),
+      long_description=io.open('README.rst', 'r', encoding='utf-8').read(),
       classifiers=[
           'Development Status :: 4 - Beta',
           'Environment :: Web Environment',
@@ -47,6 +79,7 @@ setup(name='pylw',
           'Operating System :: POSIX',
           'Topic :: Internet :: WWW/HTTP :: WSGI',
           'Topic :: Software Development :: Libraries :: Application Frameworks',
+          'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
           'Programming Language :: Python',
           'Programming Language :: Python :: Implementation :: CPython',
           'Programming Language :: Python :: 2.6',
@@ -57,8 +90,8 @@ setup(name='pylw',
       author='Michael Gugino',
       author_email='mike@funwithlinux.net',
       tests_require=['pytest'],
-      cmdclass={'test': PyTest},
+      cmdclass=cmdclass,
       license='Apache',
       packages=['pylw'],
-      ext_modules = [module1, module2],
+      ext_modules = ext_modules,
       zip_safe=False)
